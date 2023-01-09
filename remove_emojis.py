ac3970934b7c9747a97151c03190d175e9cc12c5
1400584 <1400584@fahm.kavosh.org>
2023-01-09 19:56:57 +0330

    remove emojis

--- remove_emojis.py
+++ remove_emojis.py
@@ -3,14 +3,16 @@ import regex as re
 
 with open("emoji_pattern.txt", "rb") as file:
     emojis = file.read().decode("utf-8")
-
 emoji_pattern = re.compile(emojis, flags=re.UNICODE)
 
+def remove_emojis(text):
+    # remove emojies
+    clean_text = emoji_pattern.sub(r'', text)
+    return clean_text
 text = "فرحتي أن خنيدة سوت لايك بالغلط من قوة عاطفتها للمعارضة😭  إلى أين يا خنيدة💔😭  عقدة النقص💔"
 
-# remove emojies
-clean_text = emoji_pattern.sub(r'', text)
+
 
 print("Original text: ", text)
-print("Cleaned text: ", clean_text)
+print("Cleaned text: ", remove_emojis(text))
 
